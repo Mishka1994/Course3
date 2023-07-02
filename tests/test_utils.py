67582e4246b0095dc4151e1dbff9553c1f2abf1f
Mihail Samofalov <misasamofalov611@gmail.com>
2023-07-03 05:36:24 +0700

    add all

---
+++ tests/test_utils.py
@@ -0,0 +1,91 @@
+from src import utils
+
+
+def test_date_operation():
+    assert utils.get_date_operation(operation={
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount": {
+            "amount": "31957.58",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589"
+    }) == "26.08.2019"
+
+
+def test_get_description_operation():
+    assert utils.get_description_operation(operation={
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount": {
+            "amount": "31957.58",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589"
+    }) == "Перевод организации"
+
+
+def test_get_name_and_number_from():
+    assert utils.get_name_and_number_product_from(operation={
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount": {
+            "amount": "31957.58",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589"
+    }) == ('Maestro', '1596 83** **** 5199')
+
+
+def test2_get_name_and_number_from():
+    assert utils.get_name_and_number_product_from(operation={
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {
+            "amount": "9824.07",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702"
+    }) == ('Счет', '**6952')
+
+def test_get_name_and_number_to():
+    assert utils.get_name_and_number_product_to(operation={
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {
+            "amount": "9824.07",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702"
+    }) == ('Счет', '**6702')
+
